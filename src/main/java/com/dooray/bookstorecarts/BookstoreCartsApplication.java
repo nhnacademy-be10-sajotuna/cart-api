832957dc96802dd8a514776119d037baa50e6d24
임동혁 <kingdong710@naver.com>
2025-06-09 16:31:05 +0900

    init

---
+++ src/main/java/com/dooray/bookstorecarts/BookstoreCartsApplication.java
@@ -0,0 +1,13 @@
+package com.dooray.bookstorecarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreCartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreCartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dooray/bookstorecarts/BookstoreCartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dooray.bookstorecarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreCartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
