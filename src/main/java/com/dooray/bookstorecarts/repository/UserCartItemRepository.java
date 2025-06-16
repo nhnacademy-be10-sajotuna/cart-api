3ed187a2db9b007b9185e98ff3090a1d0a042491
임동혁 <kingdong710@naver.com>
2025-06-16 17:12:24 +0900

    [feat]: 쿼리 dsl 추가

--- src/main/java/com/dooray/bookstorecarts/repository/UserCartItemRepository.java
+++ src/main/java/com/dooray/bookstorecarts/repository/UserCartItemRepository.java
@@ -3,10 +3,11 @@ package com.dooray.bookstorecarts.repository;
 import com.dooray.bookstorecarts.entity.Cart;
 import com.dooray.bookstorecarts.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
 
-public interface UserCartItemRepository extends JpaRepository<CartItem, Long> {
+public interface UserCartItemRepository extends JpaRepository<CartItem, Long>, QuerydslRepository {
     List<CartItem> findByCart(Cart cart);
-    CartItem findByCartAndBookId(Cart cart, Long bookId);
+//    CartItem findByCartAndBookId(Cart cart, Long bookId);
 }
