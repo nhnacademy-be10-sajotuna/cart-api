c7263fadd5a3eab8d8966682e72b405ee3ec43e7
임동혁 <kingdong710@naver.com>
2025-06-23 10:55:31 +0900

    [feat]: 레디스 세션 30분뒤 자동삭제

--- src/main/java/com/dooray/bookstorecarts/BookstoreCartsApplication.java
+++ src/main/java/com/dooray/bookstorecarts/BookstoreCartsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class BookstoreCartsApplication {
 
     public static void main(String[] args) {
