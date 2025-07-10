c1eceda2f422e6f422ac7462814bb00d95b5e9c6
sanikani <sanikani@naver.com>
2025-07-10 13:59:28 +0900

    [fix] : 생성자 추가

--- src/main/java/com/dooray/bookstorecarts/feign/BookSummaryResponse.java
+++ src/main/java/com/dooray/bookstorecarts/feign/BookSummaryResponse.java
@@ -3,12 +3,14 @@ package com.dooray.bookstorecarts.feign;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookSummaryResponse {
     private String isbn;
     private String title;
