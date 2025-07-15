bcb8585017fe5d6a85e6844d4e62dee47a6ebcfb
임동혁 <kingdong710@naver.com>
2025-07-15 14:14:26 +0900

    [feat]: 코드 리팩토링

--- src/main/java/com/dooray/bookstorecarts/controller/CartController.java
+++ src/main/java/com/dooray/bookstorecarts/controller/CartController.java
@@ -15,7 +15,7 @@ public class CartController {
 
     @PostMapping("/merge")  // 비회원에서 로그인한 순간 세션 장바구니와 회원 장바구니를 병합(프론트에서 로그인성공후 병합 요청)
     public ResponseEntity<CartResponse> mergeCarts(@RequestHeader(value = "X-User-Id") Long userId,
-                                                   @RequestHeader(value = "X-Guest-Cart-Id") String cartId) {
+                                                   @RequestHeader(value = "X-Guest-Cart-Id", required = false) String cartId) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(cartService.mergeCarts(userId, cartId));
